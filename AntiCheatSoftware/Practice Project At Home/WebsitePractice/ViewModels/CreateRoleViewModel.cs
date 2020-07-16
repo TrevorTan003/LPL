ce77cab3a2b61f24b95781300f676f42ba346624
Trevor <61568401+TrevorTan003@users.noreply.github.com>
2020-07-16 16:36:40 +1200

    Some of the stuff
    couldnt add all the files

---
+++ AntiCheatSoftware/Practice Project At Home/WebsitePractice/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LPL.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Role Name is required")]
+        [DisplayName("Role")]
+        public string RoleName { get; set; }
+    }
+}
